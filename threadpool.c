0798fb2e642c2df3ef17fee0f223a9a3ab31db60
gerardd33 <gerardd@interia.pl>
2020-01-07 19:44:15 +0100

    Final pool

--- threadpool.c
+++ threadpool.c
@@ -1,5 +1,4 @@
 #include "threadpool.h"
-#include <stdio.h>
 
 bool queue_empty(queue_t* queue) {
 	return queue->begin == NULL;
@@ -14,10 +13,11 @@ queue_node_t* queue_pop(queue_t* queue) {
 	return node;
 }
 
-bool queue_push(queue_t* queue, runnable_t value) {
+// Returns 0 if successful, -1 otherwise.
+int queue_push(queue_t* queue, runnable_t value) {
 	queue_node_t* new_node = malloc(sizeof(queue_node_t));
 	if (new_node == NULL)
-		return false;
+		return -1;
 	new_node->value = value;
 	new_node->next = NULL;
 
@@ -29,7 +29,7 @@ bool queue_push(queue_t* queue, runnable_t value) {
 		queue->end = new_node;
 	}
 
-	return true;
+	return 0;
 }
 
 void queue_clear(queue_t* queue) {
@@ -46,17 +46,21 @@ void queue_clear(queue_t* queue) {
 
 void* worker(void* data) {
 	thread_pool_t *pool = data;
+	int err;
+
 	while (1) {
-		pthread_mutex_lock(&(pool->security));
+		if ((err = pthread_mutex_lock(&(pool->security))) != 0)
+			exit(1);
 		if (queue_empty(pool->job_queue)) {
 			if (pool->interrupted)
 				break;
-			else
-				pthread_cond_wait(&(pool->job_to_do), &(pool->security));
+			else if ((err = pthread_cond_wait(&(pool->job_to_do), &(pool->security))) != 0)
+				exit(1);
 		}
 
 		queue_node_t *job_node = queue_pop(pool->job_queue);
-		pthread_mutex_unlock(&(pool->security));
+		if ((err = pthread_mutex_unlock(&(pool->security))) != 0)
+			exit(1);
 
 		if (job_node) {
 			job_node->value.function(job_node->value.arg, job_node->value.argsz);
@@ -64,7 +68,8 @@ void* worker(void* data) {
 		}
 	}
 
-	pthread_mutex_unlock(&(pool->security));
+	if ((err = pthread_mutex_unlock(&(pool->security))) != 0)
+		exit(1);
 	return NULL;
 }
 
@@ -73,17 +78,25 @@ int thread_pool_init(thread_pool_t* pool, size_t num_threads) {
 	pool->no_threads = num_threads;
 
 	queue_t* new_queue = malloc(sizeof(queue_t));
+	if (new_queue == NULL)
+		return -1;
 	new_queue->begin = NULL;
 	new_queue->end = NULL;
 	pool->job_queue = new_queue;
 
-	pthread_mutex_init(&(pool->security), NULL);
-	pthread_cond_init(&(pool->job_to_do), NULL);
+	int err;
+	if ((err = pthread_mutex_init(&(pool->security), NULL)) != 0)
+		exit(1);
+	if ((err = pthread_cond_init(&(pool->job_to_do), NULL)) != 0)
+		exit(1);
 
 	pool->threads = malloc(sizeof(pthread_t) * num_threads);
+	if (pool->threads == NULL)
+		return -1;
 
 	for (size_t i = 0; i < num_threads; ++i) {
-		pthread_create(pool->threads + i, NULL, worker, pool);
+		if ((err = pthread_create(pool->threads + i, NULL, worker, pool) != 0))
+			exit(1);
 	}
 
 	return 0;
@@ -93,35 +106,48 @@ void thread_pool_destroy(thread_pool_t* pool) {
 	if (pool == NULL)
 		return;
 
-	pthread_mutex_lock(&(pool->security));
+	int err;
+	if ((err = pthread_mutex_lock(&(pool->security))) != 0)
+		exit(1);
 	pool->interrupted = true;
-	pthread_cond_broadcast(&(pool->job_to_do));
-	pthread_mutex_unlock(&(pool->security));
+	if ((err = pthread_cond_broadcast(&(pool->job_to_do))) != 0)
+		exit(1);
+	if ((err = pthread_mutex_unlock(&(pool->security))) != 0)
+		exit(1);
 
 	void* tmp_res;
 	for (size_t i = 0; i < pool->no_threads; ++i) {
-		pthread_join(pool->threads[i], &tmp_res);
+		if ((err = pthread_join(pool->threads[i], &tmp_res)) != 0)
+			exit(1);
 	}
 
 	free(pool->threads);
 	queue_clear(pool->job_queue);
-	pthread_mutex_destroy(&(pool->security));
-	pthread_cond_destroy(&(pool->job_to_do));
+	if ((err = pthread_mutex_destroy(&(pool->security))) != 0)
+		exit(1);
+	if ((err = pthread_cond_destroy(&(pool->job_to_do))) != 0)
+		exit(1);
 }
 
 int defer(thread_pool_t* pool, runnable_t runnable) {
 	if (pool == NULL)
 		return -1;
 
-	pthread_mutex_lock(&(pool->security));
+	int err;
+	if ((err = pthread_mutex_lock(&(pool->security))) != 0)
+		exit(1);
 	if (pool->interrupted) {
-		pthread_mutex_unlock(&(pool->security));
+		if ((err = pthread_mutex_unlock(&(pool->security))) != 0)
+			exit(1);
 		return -1;
 	}
 
-	queue_push(pool->job_queue, runnable);
-	pthread_cond_broadcast(&(pool->job_to_do));
-	pthread_mutex_unlock(&(pool->security));
+	if ((err = queue_push(pool->job_queue, runnable)) != 0)
+		return -1;
+	if ((err = pthread_cond_broadcast(&(pool->job_to_do))) != 0)
+		exit(1);
+	if ((err = pthread_mutex_unlock(&(pool->security))) != 0)
+		exit(1);
 
 	return 0;
 }
